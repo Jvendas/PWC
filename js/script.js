fc7ad1daae7eb4b3e3edc83ab3e0f816b26d2a75
danielarroteia9898 <73316307+danielarroteia9898@users.noreply.github.com>
2021-01-22 19:14:11 +0000

    JSON Updates

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
     // define api keys
 var apiKey = '1da21aadb2d8a4a01756f791d66a9dc6';
 var apiSecret = 'a9af45ac5f7059fc01ffadf7bb3ec8a9';
-var cloneMedia = $('.music-info').clone();
+//cloneMedia = $('.music-info').clone();
+var cloneMedia = $('.media').clone();
 
 $('#btnSearch').on('click', function(){
 
+
     var valorPesquisa = $('#pesquisa').val();
-    $('.musicas-title').text('Resultados de Músicas para "'+ valorPesquisa + '"');
+    $('.panel-title').text('Resultados de Músicas para "'+ valorPesquisa + '"');
 
     $('.lista').html(' ');
 
@@ -15,16 +17,16 @@ $('#btnSearch').on('click', function(){
         method:"GET",
         url:"http://ws.audioscrobbler.com/2.0/?method=track.search&track="+valorPesquisa+"e&api_key="+apiKey+"&format=json"
     }).done(function(msg){
-        console.log('msg');
         console.log(msg);
 
 
-        msg.Search.forEach(function(result){
-            var liMedia= cloneMedia.clone();
-            $('#img', liMedia).attr("src", trackmatches.image);
-            $('.nome-musica', liMedia).text(trackmatches.name);
-            $('.nome-artista', liMedia).text(trackmatches.artist);
-            $('.lista').append(liMedia);
+       msg.results.trackmatches.track.forEach(function(result){
+            var liMusica  = cloneMedia.clone();
+            //$('#img', liMusica).attr("src", trackmatches.image);
+            $('.title',liMusica).text("Slipknotsa"+result.name);
+            $('.ano', liMusica).text("Slipknotsa"+result.name);
+            $('.media-list').append(liMusica);
+           console.log(result.name);
         })
     })
 
