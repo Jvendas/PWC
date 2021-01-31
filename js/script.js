6aa827b2bf11fc5a25a030527de981469ca16499
danielarroteia9898 <73316307+danielarroteia9898@users.noreply.github.com>
2021-01-31 16:56:25 +0000

    favoritos

--- js/script.js
+++ js/script.js
@@ -23,17 +23,13 @@ $(document).ready(function() {
             $('.title-music',liTop).text(result.name);
             $('.title-artist', liTop).text(result.artist.name);
             $('.pagina-detalhes', liTop).prop("href","./detalhes.html?var1="+result.name+"&var2="+result.artist.name);
-            $('#fav',liTop).prop("value",result.name);
             $('.top-list').append(liTop);
         })
     })
 
 });
 
-function adicionar_musica() {
-    var musica = $("#fav").val();
-    console.log(musica);
-} 
+
        
 //Pesquisa
 $('#btnSearch').click(function(){
@@ -86,6 +82,7 @@ $('#btnSearch').click(function(){
             $('.title-musica',liMusica).text(result.name);
             $('.artista-musica', liMusica).text(result.artist);
             $('.pagina-detalhes', liMusica).prop("href","./detalhes.html?var1="+result.name+"&var2="+result.artist);
+
             $('.music-list').append(liMusica);
         })
     })
--- slider_js/slider.js
+++ slider_js/slider.js
@@ -1,9 +1,4 @@
-/* full-screen-jquery-content-slider
- * <https://github.com/jacobxperez/full-screen-jquery-content-slider>
- * Copyright (C) 2020 Jacob Perez <jacobxperez@gmx.com>
- * Licensed under the Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0
-------------------------------------------------------------------------------*/
+
 $(function() {
 
 	// Slider
